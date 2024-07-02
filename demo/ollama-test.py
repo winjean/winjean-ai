822962c137b902569696b7b2805cce2d3d8d36a7
winjean <wangjian@chinaliancheng.com>
2024-07-02 17:57:52 +0800

    Default Changelist

--- demo/ollama-test.py
+++ demo/ollama-test.py
@@ -1,11 +1,19 @@
 from langchain.schema.output_parser import StrOutputParser
 from langchain_community.llms import Ollama
+from langchain.prompts import PromptTemplate
+from flask import Flask, request, render_template
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger('langchain.retrievers.multi_query').setLevel(logging.INFO)
 
 
-def main():
-    from langchain.prompts import PromptTemplate
-
-    ollama_llm = Ollama(base_url="http://localhost:11434", model="qwen:14b")
+def get_question_answer(question: str) -> str:
+    logging.info(f"question: {question}")
+    # ollama_llm = Ollama(base_url="http://localhost:11434", model="qwen:14b")
+    ollama_llm = Ollama(base_url="http://10.20.7.103:11434", model="qwen2:7b")
 
     template = """你是一个知识渊博的助手。回答问题时，请保持简洁明了。
         问题: {question}"""
@@ -16,10 +24,23 @@ def main():
     chain = prompt | ollama_llm | output
 
     # 调用Chain获取回答
-    question = "如何学好 langchain？"
-    response = chain.invoke({"question": question})
-    print(response)
+    # question = "如何学好 langchain？"
+    return chain.invoke({"question": question})
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+
+        # 接收用户输入作为问题
+        question = request.form.get('question')
+        result = get_question_answer(question)
+
+        # 把大模型的回答结果返回网页进行渲染
+        return render_template('index.html', result=result)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', debug=True, port=5000)
--- md/ollama.md
+++ md/ollama.md
@@ -1,1 +1,12 @@
+https://ollama.com/
+username: winjean0825
+password: winjean0825
+
+模型存放地址:
+OLLAMA_MODELS
+
+ollama服务地址
+OLLAMA_HOST --> http://ip:11434
+
 ollama run qwen:14b
+ollama run qwen2:7b
