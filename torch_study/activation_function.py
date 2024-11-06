6dc1251eb5bfadc5ff27f0814688812f1339e6e3
winjean <19548901@qq.com>
2024-11-06 15:02:19 +0800

    提交

--- torch_study/activation_function.py
+++ torch_study/activation_function.py
@@ -1,25 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 定义激活函数
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def tanh(x):
     return np.tanh(x)
 
+
 def relu(x):
     return np.maximum(0, x)
+
 
 def leaky_relu(x, alpha=0.01):
     return np.maximum(alpha * x, x)
 
+
 def elu(x, alpha=1.0):
     return np.where(x > 0, x, alpha * (np.exp(x) - 1))
+
 
 def softmax(x):
     exp_x = np.exp(x - np.max(x))  # 防止数值溢出
     return exp_x / np.sum(exp_x)
+
 
 # 生成数据
 x = np.linspace(-5, 5, 100)
